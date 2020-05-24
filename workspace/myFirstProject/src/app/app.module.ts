924c4a658071eaa29a1e1ef3598cbc9865b5e948
MMC <michaelconfortini@gmail.com>
2020-05-24 13:28:49 +0200

    Affiche et masque les images

--- workspace/myFirstProject/src/app/app.module.ts
+++ workspace/myFirstProject/src/app/app.module.ts
@@ -1,34 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './layout/header/header.component';
-import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
-import { SharedComponent } from './shared/shared.component';
-import { UiComponent } from './shared/ui/ui.component';
 import { StarComponent } from './shared/ui/star/star.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LayoutComponent,
+    AppComponent,   
     HeaderComponent,
-    ProductComponent,
     ProductListComponent,
     ProductDetailComponent,
-    SharedComponent,
-    UiComponent,
     StarComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
