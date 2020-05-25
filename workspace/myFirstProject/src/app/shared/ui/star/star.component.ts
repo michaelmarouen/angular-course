7ce3ee6580fab00b0fff5e9ea0b890f10139fb97
MMC <michaelconfortini@gmail.com>
2020-05-26 00:42:03 +0200

    Intégration etoile produit liste + creatin output avec listener et use two-way binding

--- workspace/myFirstProject/src/app/shared/ui/star/star.component.ts
+++ workspace/myFirstProject/src/app/shared/ui/star/star.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnInit {
+export class StarComponent  {
+  
+  @Input() rating : number;  
+  @Output() ratingChanged : EventEmitter<number> = new EventEmitter<number>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  clickIt(value : number) :void{ 
+      this.rating = value 
+      this.ratingChanged.emit(value)
   }
-
+ 
 }
