bcf99aabeb0a7912daa3da82a8b021b95040e88b
MMC <michaelconfortini@gmail.com>
2020-05-28 17:55:46 +0200

    Ajout des boutons pour suivant et retour + ajout css activated

--- workspace/myFirstProject/src/app/product/product-detail/product-detail.component.ts
+++ workspace/myFirstProject/src/app/product/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent  {
-  
+  public title: string = "Détails produits"
+  constructor(private router: Router){}
+
+  goToProducts(){
+    this.router.navigate(['/products'])
+  }
 }
