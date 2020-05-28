206b065090607fa31aa65b0750e0662347492235
MMC <michaelconfortini@gmail.com>
2020-05-28 12:43:18 +0200

    Define the route /products

--- workspace/myFirstProject/src/app/app-routing.module.ts
+++ workspace/myFirstProject/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; 
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'welcome', 'component': WelcomeComponent},
-  { path: 'products', 'component': ProductComponent}
+  { path: 'products', 'component': ProductComponent, children:[
+      { path: '', component: ProductListComponent },
+      { path: ':id', component: ProductDetailComponent },
+      { path: ':id/edit', component: ProductDetailComponent}
+  ]
+  } // Will be changed when we'll use forms}
 ]; 
 
 @NgModule({
