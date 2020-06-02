93b86be7c6df482fbbaf0099909a0ffb04112a06
MMC <michaelconfortini@gmail.com>
2020-06-02 12:07:15 +0200

    1.2 Build a local variable currentId$ which is an Observable<number> from the router paramMap source

--- workspace/myFirstProject/src/app/product/product-detail/product-detail.component.ts
+++ workspace/myFirstProject/src/app/product/product-detail/product-detail.component.ts
@@ -9,7 +9,15 @@ import { ProductService } from 'src/app/shared/model/product.service';
 })
 export class ProductDetailComponent  {
   public title: string = "Détails produits"
-  constructor(private router: Router, public productService : ProductService, public route: ActivatedRoute){}
+  
+  constructor(private router: Router, public productService : ProductService, public route: ActivatedRoute){
+    let currentId$ = route.paramMap.pipe(
+      map((param: ParamMap) => param.get('id')), // Retrieve the param with key 'id'
+      filter((id?: string) => id !== null), // Filter to not getting null value
+      map((id: string) => Number(id)) // Cast the param (string) into a number
+    )
+
+  }
 
   goToProducts(){
     this.router.navigate(['/products'])
