c20f0968020432bac5a6864651749af021ea4ee4
MMC <michaelconfortini@gmail.com>
2020-06-04 01:11:36 +0200

    Déclaration FormGroup et Validator et Assignation de tous les champs.

--- workspace/myFirstProject/src/app/product/product-edit/product-edit.component.ts
+++ workspace/myFirstProject/src/app/product/product-edit/product-edit.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const HTTP_URL_PATTERN: string =
+  '^((http[s]?):\\/)\\/?([^:\\/\\s]+)((\\/\\w+)*)([\\w\\-\\.]+[^#?\\s]+)(.*)?(#[\\w\\-]+)?$'
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent implements OnInit {
+export class ProductEditComponent{
 
-  constructor(fb: FormBuilder) { }
-
-  ngOnInit(): void {
+  public productForm: FormGroup
+  
+  constructor(fb: FormBuilder) { 
+    this.productForm = fb.group({
+      id: [null],
+      productName: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(80)]],
+      productCode: [''],
+      releaseDate: [new Date()],
+      description: [''],
+      price: [0, Validators.min(0)],
+      starRating: [0, [Validators.min(0), Validators.max(5)]],
+      imageUrl: ['', Validators.pattern(HTTP_URL_PATTERN)]
+    })     
   }
+
 
 }
