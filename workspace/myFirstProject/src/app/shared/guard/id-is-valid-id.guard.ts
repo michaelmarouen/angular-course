aba47f5acf0f0afdd736a11c7fdbbd1192b7b1a4
MMC <michaelconfortini@gmail.com>
2020-06-04 01:04:42 +0200

    Guard vérification si ID est un nombre ou pas

--- workspace/myFirstProject/src/app/shared/guard/id-is-valid-id.guard.ts
+++ workspace/myFirstProject/src/app/shared/guard/id-is-valid-id.guard.ts
@@ -9,7 +9,16 @@ export class IdIsValidIdGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+    const id = Number(next.paramMap.get('id'));
+    console.log(id);
+    if(!isNaN(id) && id >0){
+      return true;
+    }else{
+      this.router.navigateByUrl('/products')
+      return false
+    }
+    
   }
   
 }
